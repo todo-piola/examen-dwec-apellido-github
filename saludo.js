7e25b818b0b79c43cbc13228babe95d7682d71b2
Franco Benavides <benavidesgarciafranco@gmail.com>
2025-10-01 09:18:17 +0200

    añadir saludo inicial con nombre y apellidos

---
+++ saludo.js
@@ -0,0 +1,6 @@
+let Nombre = "Fran"
+let Apellido1 = ""
+let Apellido2 = ""
+// JavaScript no permite acceder directamente al nombre de una variable desde su valor,
+// porque ese vínculo se pierde al ejecutar el código
+console.log("Hola " + "Nombre " + "Apellido1 " + "Apellido2!")
