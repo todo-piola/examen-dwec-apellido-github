3b26aabdcc3c9bbeb51121575e4442a486fcdfec
Franco Benavides <benavidesgarciafranco@gmail.com>
2025-10-01 09:19:24 +0200

    añadido mensaje de motivación

--- saludo.js
+++ saludo.js
@@ -4,3 +4,4 @@ let Apellido2 = ""
 // JavaScript no permite acceder directamente al nombre de una variable desde su valor,
 // porque ese vínculo se pierde al ejecutar el código
 console.log("Hola " + "Nombre " + "Apellido1 " + "Apellido2!")
+console.log("Voy a aprobar el módulo de DWEC !")
